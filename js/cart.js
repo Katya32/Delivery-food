66ce190966a5868b413685366ba5331840361562
Katya Kondratchuk <68007177+Katya32@users.noreply.github.com>
2021-11-09 01:08:14 +0200

    delivery

---
+++ js/cart.js
@@ -0,0 +1,137 @@
+const buttonCart = document.getElementById("cart-button");
+const modalCart = document.querySelector(".modal-cart");
+const body = modalCart.querySelector(".modal-body");
+const close = modalCart.querySelector(".close");
+const modalFooter = modalCart.querySelector(".modal-footer");
+
+const getFromStore = (name) => localStorage.getItem(name);
+const setFromStore = (name, value) => localStorage.setItem(name, value);
+const removeFromStore = (name) => localStorage.removeItem(name);
+
+const openModal = () => modalCart.classList.add("is-open");
+const closeModal = () => modalCart.classList.remove("is-open");
+
+const totalPrice = (data) => {
+  const price = modalCart.querySelector(".modal-pricetag");
+
+  price.textContent = `${[...data].reduce(
+    (accumulator, currentValue) =>
+      accumulator + currentValue.price * currentValue.count,
+    0
+  )} ₽`;
+};
+
+const resetCart = () => {
+  body.innerHTML = "";
+  removeFromStore("cart");
+  closeModal();
+};
+
+const incrementCount = (id) => {
+  const cartArray = JSON.parse(getFromStore("cart"));
+  cartArray.map((item) => {
+    if (item.id === id) {
+      item.count++;
+    }
+  });
+
+  setFromStore("cart", JSON.stringify(cartArray));
+  renderItems(cartArray);
+};
+
+const decrementCount = (id) => {
+  const cartArray = JSON.parse(getFromStore("cart"));
+  cartArray.map((item) => {
+    if (item.id === id) {
+      item.count = item.count > 0 ? item.count - 1 : 0;
+    }
+  });
+
+  setFromStore("cart", JSON.stringify(cartArray));
+  renderItems(cartArray);
+};
+
+const renderFooter = () => {
+  modalFooter.innerHTML = `
+      <span class="modal-pricetag"></span>
+      <div class="footer-buttons">
+        <button class="button button-primary submit">Оформить заказ</button>
+        <button class="button clear-cart">Отмена</button>
+      </div>
+    `;
+
+  const buttonSend = modalCart.querySelector(".submit");
+  const buttonCancel = modalCart.querySelector(".clear-cart");
+
+  buttonSend.addEventListener("click", () => {
+    const cartArray = getFromStore("cart");
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: cartArray,
+    })
+      .then((response) => {
+        if (response.ok) {
+          resetCart();
+        }
+      })
+      .catch((e) => console.error(e));
+  });
+
+  buttonCancel.addEventListener("click", resetCart);
+};
+
+const renderItems = (data) => {
+  body.innerHTML = "";
+
+  if (data) {
+    data.forEach((cartItem) => {
+      const { id, name, price, count } = cartItem;
+      const cartElem = document.createElement("div");
+      cartElem.classList.add("food-row");
+      cartElem.innerHTML = `
+          <span class="food-name">${name}</span>
+          <strong class="food-price">${price} ₽</strong>
+          <div class="food-counter">
+            <button class="counter-button btn-dec" data-index="${id}">-</button>
+            <span class="counter">${count}</span>
+            <button class="counter-button btn-inc" data-index="${id}">+</button>
+          </div>
+      `;
+
+      body.append(cartElem);
+      renderFooter();
+    });
+
+    totalPrice(data);
+  } else {
+    modalFooter.innerHTML = "";
+
+    const cartElem = document.createElement("div");
+    cartElem.classList.add("food-row");
+
+    cartElem.innerHTML = `
+        <span class="food-name">Продуктов нет!!!</span>
+      `;
+
+    body.append(cartElem);
+  }
+};
+
+body.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("btn-inc")) {
+    incrementCount(e.target.dataset.index);
+  } else if (e.target.classList.contains("btn-dec")) {
+    decrementCount(e.target.dataset.index);
+  }
+});
+
+buttonCart.addEventListener("click", () => {
+  renderItems(JSON.parse(getFromStore("cart")));
+
+  openModal();
+  console.log("fghijo;klghcgfhcjgkjlo;ojlhuhjgvjhgkl");
+});
+
+close.addEventListener("click", closeModal);
---
+++ js/menu.js
@@ -0,0 +1,80 @@
+const cardsMenu = document.querySelector(".cards-menu");
+
+const getStoreValue = (name) => localStorage.getItem(name);
+const setStoreValue = (name, value) => localStorage.setItem(name, value);
+
+const addToCart = (cartItem) => {
+  const cartArray = getStoreValue("cart")
+    ? JSON.parse(getStoreValue("cart"))
+    : [];
+
+  if (cartArray.some((item) => item.id === cartItem.id)) {
+    cartArray.map((item) => {
+      if (item.id === cartItem.id) {
+        item.count++;
+      }
+    });
+  } else {
+    cartArray.push(cartItem);
+  }
+
+  setStoreValue("cart", JSON.stringify(cartArray));
+};
+
+const renderItems = (data) => {
+  data.forEach(({ description, id, image, name, price }) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <img src="${image}" alt="${name}" class="card-image" />
+    <div class="card-text">
+        <div class="card-heading">
+        <h3 class="card-title card-title-reg">${name}</h3>
+        </div>
+        <div class="card-info">
+            <div class="ingredients">${description}</div>
+        </div>
+        <div class="card-buttons">
+            <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+            </button>
+            <strong class="card-price-bold">${price} ₽</strong>
+        </div>
+    </div>
+    `;
+    card.querySelector(".button-card-text").addEventListener("click", () => {
+      addToCart({ name, price, id, count: 1 });
+    });
+    cardsMenu.append(card);
+  });
+};
+
+const changeTitle = (restaurant) => {
+  const { kitchen, price, name, stars } = restaurant;
+  const restaurantSectionHeading = document.querySelector(".item-name");
+  restaurantSectionHeading.innerHTML = `
+      <h2 class="section-title restaurant-title">${name}</h2>
+        <div class="card-info">
+        <div class="rating">${stars}</div>
+        <div class="price">От ${price} ₽</div>
+        <div class="category">${kitchen}</div>
+      </div>
+    `;
+};
+
+if (localStorage.getItem("restaurant")) {
+  const restaurant = JSON.parse(localStorage.getItem("restaurant"));
+  changeTitle(restaurant);
+
+  fetch(`./db/${restaurant.products}`)
+    .then((res) => res.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+} else {
+  window.location.href = "/delivery-service/index.html";
+}
